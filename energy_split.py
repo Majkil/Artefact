4ca0d459e28fe60e85e00e713e34393a06ef39a7
MajkilV <michaelvassallo89@gmail.com>
2021-05-11 21:03:17 +0200

    splitting by delta energy

--- energy_split.py
+++ energy_split.py
@@ -1,21 +1,20 @@
 from utilities import *
+import math
 
 
-def Split(audio, hop_length, frame_length, min_duration=10):
+def Split(audio, hop_length, frame_length, min_duration=10, energy_threshold=0.05):
     f = librosa.feature.rms(audio, hop_length=hop_length,
                             frame_length=frame_length).flatten()
-    rmse_diff = np.zeros_like(f)
-    rmse_diff[1:] = np.diff(f)
+
     start, end = 0, 0
     voiced = []
     n = normalize(f)
-    t = 0.02
     for x in range(len(n)):
-        if n[x] > t:
+        if n[x] > energy_threshold:
             if start == 0:
                 start = x
             if start != 0 and x < len(n):
-                if n[x+1] < t and start != x:
+                if n[x+1] < energy_threshold and start != x:
                     end = x
                     voiced.append([start, end])
                     start, end = 0, 0
@@ -27,6 +26,46 @@ def Split(audio, hop_length, frame_length, min_duration=10):
             trimmed.append(x)
 
     return trimmed
+
+
+def Split2(audio, hop_length, frame_length, sr,  min_duration=700):
+    e = librosa.pcen(audio, sr=sr)
+    d = librosa.feature.delta(e)
+    start, end = 0, 0
+    boundaries = []
+    positive = True
+    boundaries.append(0)
+    #diff = 0.8
+    # calulate delta Y  = m
+    # dy = sqrt(m^2)
+    # take largest dy as boundary
+    # take lowest dy as turning point1
+    # take next value close to boundary value as closing value
+    for c in range(len(d)):
+        if d[c]<d[c-1] and d[c]< d[c+1] and boundaries[-1]+min_duration <= c:
+            boundaries.append(c) 
+    x = []
+    if  len(d) - boundaries[-1]<min_duration:
+        boundaries[-1] = len(d)
+    if boundaries[-1] == boundaries[-2]:
+        boundaries.pop()
+    # gradients = []
+    # for y in range(len(d)):
+    #     if y < len(d)-1:
+    #         m = d[y]-d[y+1]
+    #         dy = math.sqrt(m**2)
+    #         gradients.append(dy)
+    # highest_gradient = 0
+
+    # for i in range(len(gradients)):
+    #     if i < len(gradients)-1 and boundaries[-1]+min_duration = i:
+    #         if gradients[i] > gradients[i+1] and gradients[i] > highest_gradient:
+    #             highest_gradient = gradients[i]
+    #             boundaries.append(i)
+    #         if gradients[i] > gradients[i+1] and gradients[i] < highest_gradient:
+    #             boundaries.append(i)
+    #             highest_gradien = 0
+    return boundaries
 
 
 def energies(audio, hop_length, frame_length):
--- splitTest.py
+++ splitTest.py
@@ -1,4 +1,5 @@
-#In[]:
+# In[]:
+from energy_split import *
 import librosa
 import math
 import librosa.display as display
@@ -10,22 +11,34 @@ from sklearn.mixture import GaussianMixture
 import fastaudio.core.signal as fcs
 import GetTranscription
 
-#In[]:
-lvpath ="E:\Datasets\Voice\Librivox\dev\LibriSpeech\dev-clean"
-libri_train ="E:\Datasets\Voice\LibriSpeech"
-mcvpath ="E:\Datasets\Voice\Mozilla Common Voice\en\cv-corpus-6.1-2020-12-11\en"
+lvpath = "E:\Datasets\Voice\Librivox\dev\LibriSpeech\dev-clean"
+libri_train = "E:\Datasets\Voice\LibriSpeech"
+mcvpath = "E:\Datasets\Voice\Mozilla Common Voice\en\cv-corpus-6.1-2020-12-11\en"
 single_word = "./samples/but bowl.wav"
 
-clips =fcs.get_audio_files(libri_train)
-print(len(clips))
-clip= clips[6576]
-audio,sr = librosa.load(clip)
+clips = fcs.get_audio_files(libri_train)
+sr = 16000
+hop_length = int(sr/200)
+frame_length = int(hop_length*2)
+clips = fcs.get_audio_files(libri_train)
+clip = clips[2402]
+audio, sr = librosa.load(clip, sr=sr)
 print(clip)
 print(GetTranscription.get_file_transcript(clip))
 print(audio.shape)
+#In[]
+threshold = 0.5
+segments = Split(audio,hop_length,frame_length,min_duration=3, energy_threshold=0.05)
+segment = segments[1]
+print(segments)
+print(len(segments))
+print(segment)
 
-#In[]:
+# In[]
 
-def normalize(x, axis=0):
-    return sklearn.preprocessing.minmax_scale(x, axis=axis)
+part = audio[segment[0]*hop_length:segment[1] * hop_length]
+segments = Split2(part, hop_length, frame_length, sr, min_duration=hop_length*8)
+print(segments)
+print(len(segments))
+
 # %%
